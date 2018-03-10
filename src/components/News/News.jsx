1613650c4f6b54d11537083c38be3f95de6b6910
Jason Smith <s.jason@shaw.ca>
2018-03-10 15:03:14 -0800

    Adding links and images to news sources

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -12,15 +12,24 @@ export class News extends Component{
         super(props)
     
         this.state = {
-          coin: []
+            news: [],
+            news_img: [],
+            news_url: []
         }
       }
     // Import data
     componentDidMount () {
         axios.get('https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=5c922eb873824ce1a0c6e3e1f83f3dad')
           .then(res => {
-            const coin = res.data[0]
-            this.setState({ coin })
+            for (var i = 0; i < 6; i++) {
+                this.state.news.push(res.data.articles[i].title)
+                this.state.news_img.push(res.data.articles[i].urlToImage)
+                this.state.news_url.push(res.data.articles[i].url)
+            }
+          })
+          .catch(err => {
+            this.state.news.push("Unable to load news data")
+            console.log(err);
           })
       }
 
@@ -35,20 +44,28 @@ export class News extends Component{
         const edit = (<Tooltip id="edit_tooltip">Edit Task</Tooltip>);
         const remove = (<Tooltip id="remove_tooltip">Remove</Tooltip>);
         const news_title = [
-            'Crypto Exchange Bittrex: We’re Compliant With SEC’s ICO Rules',
-            'Russia is Considering Relaxed Rules for Cryptocurrency Adopters',
-            'Cryptocurrency Hedge Fund Raises Millions of Dollars From Major VCs',
-            'Cryptocurrency Will Take Power Away from Central Banks: Steve Bannon',
-            'Bitcoin: Doomed for Success',
-            'The First Blockchain-Backed Presidential Election Just Took Place in Sierra Leone'
+            this.state.news[0],
+            this.state.news[1],
+            this.state.news[2],
+            this.state.news[3],
+            this.state.news[4],
+            this.state.news[5]
+        ];
+        const news_url = [
+            this.state.news_url[0],
+            this.state.news_url[1],
+            this.state.news_url[2],
+            this.state.news_url[3],
+            this.state.news_url[4],
+            this.state.news_url[5]
         ];
         const img_urls = [
-            'https://www.ccn.com/wp-content/uploads/2018/01/Securities-and-Exchange-Commission.jpg',
-            'https://www.ccn.com/wp-content/uploads/2017/05/Central-Bank-of-Russia.jpg',
-            'https://www.ccn.com/wp-content/uploads/2018/03/Bitcoin-dollar-roll.jpg',
-            'https://www.ccn.com/wp-content/uploads/2018/03/Steve-Bannon.jpg',
-            'https://www.ccn.com/wp-content/uploads/2018/03/hiker-celebrating-success-000055416656_Large.jpg',
-            'https://www.ccn.com/wp-content/uploads/2018/03/Sierra-Leone.jpg'
+            this.state.news_img[0],
+            this.state.news_img[1],
+            this.state.news_img[2],
+            this.state.news_img[3],
+            this.state.news_img[4],
+            this.state.news_img[5]
         ];
         var news = [];
         var number;
@@ -57,9 +74,9 @@ export class News extends Component{
             news.push(
                 <tr key={i}>
                     <td>
-                    <Thumbnail src={img_urls[i]} size="10x10" />
+                    <a href={news_url[i]}><Thumbnail src={img_urls[i]} size="10x10" /></a>
                     </td>
-                    <td>{news_title[i]}</td>
+                    <td><a href={news_url[i]}>{news_title[i]}</a></td>
                     <td className="td-actions text-right">
                         <OverlayTrigger placement="top" overlay={edit}>
                             <Button
