2a563ba20eb25e0e3fad6d70d2b053eb1bede960
Jason Smith <s.jason@shaw.ca>
2018-04-14 19:08:37 -0700

    re-configuring firebase auth

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyA4fZtPpQIMvUbDk887ul2ACUlPYNeri28',
+  authDomain: 'fantasycoin-ddbe3.firebaseapp.com',
+  databaseURL: 'https://fantasycoin-ddbe3.firebaseio.com',
+  projectId: 'fantasycoin-ddbe3',
+  storageBucket: 'fantasycoin-ddbe3.appspot.com',
+  messagingSenderId: '930678974720'
+}
