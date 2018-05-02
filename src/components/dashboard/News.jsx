bad4a6f16dc88fdb41df44cf0c6796f801a6058b
Jason Smith <s.jason@shaw.ca>
2018-05-01 23:32:15 -0700

    Fixing news API and adding new styles to the section

--- src/components/dashboard/News.jsx
+++ src/components/dashboard/News.jsx
@@ -4,66 +4,50 @@ import {Thumbnail} from 'react-bootstrap'
 // import Button from 'elements/CustomButton/CustomButton.jsx'
 import axios from 'axios'
 
+var styleTest = {
+  fontSize: '25px'
+}
+
 export class News extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      newsTitle: [],
-      newsImg: [],
-      newsURL: []
+      news: []
     }
   }
   // Import data
   componentDidMount () {
     axios.get('https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=5c922eb873824ce1a0c6e3e1f83f3dad')
       .then(res => {
-        for (var i = 0; i < 6; i++) {
-          // console.log(res.data)
-          this.state.newsTitle.push(res.data.articles[i].title)
-          this.state.newsImg.push(res.data.articles[i].urlToImage)
-          this.state.newsURL.push(res.data.articles[i].url)
-        }
+        const news = res.data.articles
+        // for (var i = 0; i < 6; i++) {
+        //   // console.log(res.data)
+        //   this.state.newsTitle.push(res.data.articles[i].title)
+        //   this.state.newsImg.push(res.data.articles[i].urlToImage)
+        //   this.state.newsURL.push(res.data.articles[i].url)
+        // }
+        this.setState({ news })
       })
       .catch(err => {
-        this.state.newsTitle.push('Unable to load news data')
+        this.setState({ news: 'Unable to load news data' })
         console.log(err)
       })
   }
 
   render () {
-    const newsTitle = [
-      this.state.newsTitle[0],
-      this.state.newsTitle[1],
-      this.state.newsTitle[2],
-      this.state.newsTitle[3],
-      this.state.newsTitle[4],
-      this.state.newsTitle[5]
-    ]
-    const newsURL = [
-      this.state.newsURL[0],
-      this.state.newsURL[1],
-      this.state.newsURL[2],
-      this.state.newsURL[3],
-      this.state.newsURL[4],
-      this.state.newsURL[5]
-    ]
-    const newsImg = [
-      this.state.newsImg[0],
-      this.state.newsImg[1],
-      this.state.newsImg[2],
-      this.state.newsImg[3],
-      this.state.newsImg[4],
-      this.state.newsImg[5]
-    ]
+    // console.log(this.state.news)
+    var i = 0
     var news = []
-    for (var i = 0; i < newsTitle.length; i++) {
+    for (var newsObj in this.state.news) {
+      if (i === 6) break
+      i++
       news.push(
         <tr key={i}>
           <td>
-            <a href={newsURL[i]}><Thumbnail src={newsImg[i]} size='10x10' /></a>
+            <a href={this.state.news[newsObj].url}><Thumbnail src={this.state.news[newsObj].urlToImage} /></a>
           </td>
-          <td><a href={newsURL[i]}>{newsTitle[i]}</a></td>
+          <td><a style={styleTest}href={this.state.news[newsObj].url}>{this.state.news[newsObj].title}</a></td>
         </tr>
       )
     }
