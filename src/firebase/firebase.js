f08dac7d12360545ca7cb27eb5a634822592237b
Jason Smith <s.jason@shaw.ca>
2018-06-24 14:40:16 -0700

    fixing code standard

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,21 @@
 import * as firebase from 'firebase'
 
 const prodConfig = {
-  apiKey: "AIzaSyBLcwOXcl8bS2FaJk-Ee2yGFwdImE54rss",
-  authDomain: "fantasycoin-prod.firebaseapp.com",
-  databaseURL: "https://fantasycoin-prod.firebaseio.com",
-  projectId: "fantasycoin-prod",
-  storageBucket: "fantasycoin-prod.appspot.com",
-  messagingSenderId: "363231009012"
+  apiKey: 'AIzaSyBLcwOXcl8bS2FaJk-Ee2yGFwdImE54rss',
+  authDomain: 'fantasycoin-prod.firebaseapp.com',
+  databaseURL: 'https://fantasycoin-prod.firebaseio.com',
+  projectId: 'fantasycoin-prod',
+  storageBucket: 'fantasycoin-prod.appspot.com',
+  messagingSenderId: '363231009012'
 }
 
 const devConfig = {
-  apiKey: "AIzaSyA4fZtPpQIMvUbDk887ul2ACUlPYNeri28",
-  authDomain: "fantasycoin-ddbe3.firebaseapp.com",
-  databaseURL: "https://fantasycoin-ddbe3.firebaseio.com",
-  projectId: "fantasycoin-ddbe3",
-  storageBucket: "fantasycoin-ddbe3.appspot.com",
-  messagingSenderId: "930678974720"
+  apiKey: 'AIzaSyA4fZtPpQIMvUbDk887ul2ACUlPYNeri28',
+  authDomain: 'fantasycoin-ddbe3.firebaseapp.com',
+  databaseURL: 'https://fantasycoin-ddbe3.firebaseio.com',
+  projectId: 'fantasycoin-ddbe3',
+  storageBucket: 'fantasycoin-ddbe3.appspot.com',
+  messagingSenderId: '930678974720'
 }
 
 const config = process.env.NODE_ENV === 'production'
